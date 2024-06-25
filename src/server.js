e2c8a47c609190e38d3e213d7db8369667428282
luigi <luigigames2903@gmail.com>
2024-06-25 19:42:28 -0300

    Metodo GET

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3333;
+app.get("/message", (request, response) => {
+  response.send('Hello, world!')
+})
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
