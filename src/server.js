175d51e35008be4a16b99196405515d45a78a8f5
luigi <luigigames2903@gmail.com>
2024-06-25 20:06:38 -0300

    Query params

--- src/server.js
+++ src/server.js
@@ -5,4 +5,9 @@ app.get("/message/:id/:user", (request, response) => {
   const {id, user} = request.params;
   response.send(`Id da mensagem ${id}. Para o Usuário ${user}`)
 })
+app.get("/users", (request, response) => {
+  const {page, limit} =  request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
