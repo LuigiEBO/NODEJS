ad25b3356b8e4ef9db92816a0e2891c03705cf37
luigi <luigigames2903@gmail.com>
2024-07-11 21:29:07 -0300

    metodo post e organização de pastas

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const app = express();
-const PORT = 3333;
-app.get("/message/:id/:user", (request, response) => {
-  const {id, user} = request.params;
-  response.send(`Id da mensagem ${id}. Para o Usuário ${user}`)
-})
-app.get("/users", (request, response) => {
-  const {page, limit} =  request.query;
 
-  response.send(`Página: ${page}. Mostrar: ${limit}`)
-})
+const routes = require("./routes")
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+
+const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
