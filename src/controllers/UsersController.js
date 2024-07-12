83208f6eae699ea465ec42bcfe46237b4e6ec961
luigi <luigigames2903@gmail.com>
2024-07-11 21:34:23 -0300

    Controllers

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,10 @@
+class UsersController {
+  /* 
+  index - GET para listar varios registros;
+  show - GET para exibir um usario específico;
+  create - POST criar registros;
+  update - PUT para atualizar um registro;
+  delete - DELETE remover um registro.
+  */
+}
+module.exports = UsersController;
