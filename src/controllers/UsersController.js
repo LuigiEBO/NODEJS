232d2af67c95927a693a792bd60aa71f53fdaa8f
luigi <luigigames2903@gmail.com>
2024-07-25 16:37:33 -0300

    updates

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,23 @@
+const {hash, compare} = require('bcryptjs')
 const appError = require("../utils/appError");
 
+const sqliteConection = require('../database/sqlite');
+const { response } = require('express');
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if(!name) {
-      throw new appError("O nome e obrigatorio")
+    const database = await sqliteConection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(checkUserExists) {
+      throw new appError('Este email ja esta em uso.')
     }
-    response.status(201).json({ name, email, password })
-  }
+    
+    const hashedPassword = await hash(password, 8);
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
+
+    return response.status(201).json();
   /* 
   index - GET para listar varios registros;
   show - GET para exibir um usario específico;
@@ -16,5 +25,46 @@ class UsersController {
   update - PUT para atualizar um registro;
   delete - DELETE remover um registro.
   */
+}
+  async update(request, response) {
+    const {name, email, password, old_password} = request.body;
+    const {id } =  request.params;
+
+    const database = await sqliteConection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+    if(!user) {
+      throw new appError('Usuario não encontrado')
+    }
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new appError('Este email ja esta em uso')
+    }
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !old_password) {
+      throw  new appError("Você precisa informar sua antiga senha para cadastrar uma nova")
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword) {
+        throw new appError('A senha antiga está errada.')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password,  id]
+    );
+    return response.status(200).json();
+  }
 }
 module.exports = UsersController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,14 +7,12 @@ const usersRoute = Router()
 
 function myMiddlewere(request, response, next) {
   console.log("Você passou pelo middlewere")
-  if(!request.body.isAdmin) {
-    return response.json("User not unauthorized")
-  };
   next();
 }
 
 const usercontroller = new UsersController();
 
-usersRoute.post("/", myMiddlewere, usercontroller.create)
+usersRoute.post("/", myMiddlewere, usercontroller.create);
+usersRoute.put("/:id", usercontroller.update);
 
 module.exports = usersRoute;
