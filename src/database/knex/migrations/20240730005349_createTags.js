fbccfc40a93f0b569b841ee65cca7a76c36b6738
luigi <luigigames2903@gmail.com>
2024-07-29 22:04:59 -0300

    criando a tabela tag e link

--- src/database/knex/migrations/20240730005349_createTags.js
+++ src/database/knex/migrations/20240730005349_createTags.js
@@ -1,12 +1,9 @@
 exports.up = (knex) =>
-  knex.schema.createTable("notes", (table) => {
-    table.increments("id")
-    table.text("title")
-    table.text("description")
+  knex.schema.createTable("tags", (table) => {
+    table.increments("id");
+    table.text("name").notNullable();
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users")
-
-    table.timestamp("created_at").default(knex.fn.now())
-    table.timestamp("updated_at").default(knex.fn.now())
   })
 
-exports.down = (knex) => knex.schema.dropTable("notes")
+exports.down = (knex) => knex.schema.dropTable("tags")
