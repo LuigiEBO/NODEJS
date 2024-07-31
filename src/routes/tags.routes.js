46d6b898df1473cb47fc2d62af50341f89359374
luigi <luigigames2903@gmail.com>
2024-07-31 18:27:57 -0300

    lintando tags

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/TagsController");
+
+const tagsRoute = Router()
+
+
+const tagsController = new TagsController();
+
+tagsRoute.get("/:user_id", tagsController.index);
+
+module.exports = tagsRoute;
